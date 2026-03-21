07014a8eca6009ee5dbe7c2cc8a31297254de7a2
Abhimanyu Dwivedi <abhimanyu.ad.hmr@gmail.com>
2026-03-22 00:31:41 +0530

    Vendor Component

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import OverviewDashboard from '../order_management/components/overview.vue'
 import OrderManagement   from '../order_management/management.vue'
 import CustomerDashboard from '../order_management/components/customer.vue'
+import VendorManagement  from '../vendor_management/management.vue'
 
 const routes = [
   { path: '/',           redirect: '/dashboard' },
   { path: '/dashboard',  component: OverviewDashboard },
   { path: '/orders',     component: OrderManagement },
   { path: '/customers',  component: CustomerDashboard },
+  { path: '/vendors',    component: VendorManagement },
   // add as you build:
   // { path: '/inventory', component: () => import('../inventory_management/management.vue') },
-  // { path: '/vendors',   component: () => import('../vendor_management/management.vue') },
 ]
 
 export default createRouter({
---
+++ src/vendor_management/components/VendorTag.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="vendor-tag chip">{{ label }}</span>
+</template>
+
+<script>
+export default {
+  name: 'VendorTag',
+  props: {
+    label: { type: String, required: true }
+  }
+}
+</script>
+
+<style scoped>
+.vendor-tag {
+  background: var(--blue-dim);
+  color: var(--blue);
+}
+</style>
